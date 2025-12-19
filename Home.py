87714fd1f4a320e93409996dda5d0f2079518d06
mjandussi <112095881+mjandussi@users.noreply.github.com>
2025-12-19 09:10:12 -0300

    .

--- Home.py
+++ Home.py
@@ -57,6 +57,7 @@ st.markdown(
       <li><strong>🧩 Encerramento de Disponibilidades</strong>: Regras e verificações para processar o encerramento das disponibilidades financeiras por fonte.</li>
       <li><strong>🧾 Análise dos Arquivos SIG</strong>: Página para realizar as análises dos arquivos SIG em relação aos dados do SIAFERIO/Flexvision.</li>
       <li><strong>✅ Conferência do Plano de Contas</strong>: Página para realizar a conferência Do Plano de Contas em Relação ao Processo de Encerramento.</li>
+      <li><strong>❌ Conferência dos Programas de Trabalho e os Saldos de RPP</strong>: Página para realizar a Conferência dos Programas de Trabalho e os Saldos de RPP.</li>
     </ul>
     <h4 style="margin-top:1.1rem;">🏦 Manuais</h4>
     <ul>
